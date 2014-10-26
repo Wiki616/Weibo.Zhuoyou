2ca3185cc9d5030fdcf5a479febca98ea57dbe8c
jubin2510 <605276362@qq.com>
2014-10-26 18:26:18 +0800

    update

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from flask import render_template, flash, redirect,request
+from app import app
+import httplib
+import urllib , urllib2
+import json
+import uuid
+
+@app.route('/')
+@app.route('/index')
+def index():
+        return render_template("index.html")
+@app.route('/settings')
+@app.route('/settings/profile')
+def profile():
+        return render_template("profile.html")
+@app.route('/settings/account')
+def account():
+        return render_template("account.html")
+@app.route('/settings/friend')
+def friend():
+        return render_template("friend.html")
